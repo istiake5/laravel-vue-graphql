e7350cb4fd0ec84bd9d13077ea813aebc2410ea7
Istiake Ahamed Novel <novelistiakeahamed@gmail.com>
2023-04-06 11:35:03 +0600

    Resolver

---
+++ app/GraphQL/Queries/PostResolver.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\GraphQL\Queries;
+
+use App\Models\Post;
+
+final class PostResolver
+{
+    /**
+     * @param  null  $_
+     * @param  array{}  $args
+     */
+    public function __invoke($_, array $args)
+    {
+        
+        return Post::where('id', $args['id'])->first();
+    }
+}
